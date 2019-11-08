70c89ad3123ee005851d70a2ae2c94515ba80161
Pilch Alexander <alexander.pilch@vsf-experts.de>
2019-11-08 16:09:44 +0100

    upgrade to spring boot 2.2.1

--- latex-server/src/main/java/de/vsfexperts/latex/server/Application.java
+++ latex-server/src/main/java/de/vsfexperts/latex/server/Application.java
@@ -2,10 +2,12 @@ package de.vsfexperts.latex.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ConfigurationPropertiesScan(basePackageClasses = Application.class)
 public class Application {
 
 	public static void main(final String[] args) {
